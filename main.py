396f5c293decba4ebcb20fa4742ddc80481e241b
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-21 14:05:26 +0530

    chore: Refactor neural network architecture and training loop

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.dense_layer = nn.Sequential(
-            nn.Linear(28*28, 512),
+            nn.Linear(28*28, 1024),
+            nn.Tanh(),
+            nn.Linear(1024, 512),
             nn.ReLU(),
             nn.Linear(512, 256),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(256, 10),
             nn.ReLU()
         )
@@ -26,7 +28,6 @@ class NeuralNetwork(nn.Module):
         logits = self.dense_layer(x)
         predictions = self.softmax(logits)
         return predictions
-    
 
 
 def download_data():
@@ -46,6 +47,27 @@ def download_data():
 
     return training_data, test_data
 
+
+def train_one_epoch(model, inputs, targets, loss_fn, optimizer):
+        # Compute prediction error
+    pred = model(inputs)
+    loss = loss_fn(pred, targets )
+
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(f"Loss: {loss.item()}")
+
+
+def train(model, inputs, targets, loss_fn, optimizer, epochs):
+    for i in range(epochs):
+        print(f"Epoch {i+1}")
+        train_one_epoch(model, inputs, targets, loss_fn, optimizer)
+        print("---------------------------")
+    print("Finished training!")
+
+
 if __name__ == "__main__":
     # download data
     training_data, _ = download_data()
@@ -54,3 +76,21 @@ if __name__ == "__main__":
     # create data loader
     train_dataloader = DataLoader(training_data, batch_size=256, shuffle=True)
     print("Data loaded successfully!")
+
+    for inputs, targets in train_dataloader:
+        inputs, targets = inputs.to("cuda"), targets.to("cuda")
+
+    # create model
+    model = NeuralNetwork().to("cuda")
+    print("Model created successfully!")
+
+    # train model
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    train(model, inputs, targets, loss_fn, optimizer, epochs=1000)
+    print("Model trained successfully!")
+
+    # save model
+    torch.save(model.state_dict(), "model.pth")
+    print("Model saved successfully!")
+
