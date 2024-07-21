12906d9e42abb898382aca384271a52f76417eff
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-21 13:29:32 +0530

    .gitignore and nn class

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+
+class NeuralNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.dense_layer = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10),
+            nn.ReLU()
+        )
+        self.softmax = nn.Softmax(dim=1)
+
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.dense_layer(x)
+        predictions = self.softmax(logits)
+        return predictions
+    
+
+
 def download_data():
     training_data = datasets.MNIST(
         root="data",
@@ -22,7 +47,10 @@ def download_data():
     return training_data, test_data
 
 if __name__ == "__main__":
-
+    # download data
     training_data, _ = download_data()
     print("MNIST data downloaded successfully!")
-    
+
+    # create data loader
+    train_dataloader = DataLoader(training_data, batch_size=256, shuffle=True)
+    print("Data loaded successfully!")
