f60d2a1c876605a4392e706d24fabd89d92c5aae
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-21 13:23:16 +0530

    dataset download

--- main.py
+++ main.py
@@ -3,3 +3,26 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
+def download_data():
+    training_data = datasets.MNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
+
+    test_data = datasets.MNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor()
+    )
+
+    return training_data, test_data
+
+if __name__ == "__main__":
+
+    training_data, _ = download_data()
+    print("MNIST data downloaded successfully!")
+    
